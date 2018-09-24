0614403d4e6d1bc7c0467ea8c1c67ac4f1a3c8b8
Joshua <jwrightbluj@gmail.com>
2018-09-24 14:55:14 -0700

    Added virtualNode ID

--- virtualNode.js
+++ virtualNode.js
@@ -1,3 +1,13 @@
+
+
 class VirtualNode {
-    
+    constructor (options) {
+        this.parent = options.parent;
+        this.value = options.value;
+        this.name = options.name;
+        this.id = options.id;
+    }
+
+    //the modifiers need to be put in dynamically,
+    //as does keySubscribe
 }
